c131c2dab74a98128b7d1dce9461690b80cb8509
dschwartznyc <daniel.schwartz@ftadvisory.co>
2023-11-08 21:35:53 -0500

    run with two types of repeat

--- src/cdm_compression_timing.py
+++ src/cdm_compression_timing.py
@@ -3,14 +3,13 @@ import os
 from bson import BSON
 import zlib
 import lz4.frame
-
-from dict_comp import dict_comp
 import timeit
+from timeit import default_timer
 from prettytable import PrettyTable 
-import locale
+from statistics import mean, stdev
 
 number_of_tests = 1
-repeat          = 1000
+repeat          = 5000
 json_file_in    = 'LargeCDM.json'
 bson_file_in    = 'large_cdm.bson'
 json_file_out   = 'json_out.json'
@@ -20,6 +19,7 @@ lz4_file_out    = 'lz4_out.lz4'
 zlib_file_in    = 'large_cdm.zlib'
 zlib_file_out   = 'zlib_out.zlib'
 json_dict       = {}
+stupid_flag     = True
 
 def read_json_file():
     with open(json_file_in,'r') as r:
@@ -67,12 +67,38 @@ def format_results(result):
         '{:,.5f}'.format(result[5]),
         result[6]]
 
-def get_results (type, size, number_of_tests, repeat, read_function, write_function, json_results = None):
-    print ('tests: ' + type)
-    timings     = timeit.repeat(read_function, number=number_of_tests, repeat=repeat)
-    avg_read    = sum(timings) / len(timings)
-    timings     = timeit.repeat(write_function, number=number_of_tests, repeat=repeat)
-    avg_write   = sum(timings) / len(timings)
+def format_stats(result):
+    return [
+        result[0],
+        '{:,.5f}'.format(result[7]),
+        '{:,.5f}'.format(result[8]),
+        '{:,.5f}'.format(result[9]),
+        '{:,.5f}'.format(result[10]),
+        '{:,.5f}'.format(result[11]),
+        '{:,.5f}'.format(result[12])]
+    
+def get_stupid_results (function_name):
+    timings = [0] * repeat
+    for i in range (repeat):
+        start = default_timer()
+        function_name()
+        end   = default_timer()
+        timings[i] = (end-start)
+    return timings
+    
+def get_results (type, size, read_function, write_function, json_results = None):
+    print ('tests: ', type)
+    timings     = get_stupid_results(read_function) if stupid_flag else timeit.repeat(read_function, number=number_of_tests, repeat=repeat)
+    avg_read    = mean(timings)
+    sd_read     = stdev(timings)
+    min_read    = min(timings)
+    max_read    = max(timings)
+    timings     = get_stupid_results(write_function) if stupid_flag else timeit.repeat(write_function, number=number_of_tests, repeat=repeat)
+    avg_write   = mean(timings)
+    sd_write    = stdev(timings)
+    min_write   = min(timings)
+    max_write   = max(timings)
+    
     compression = '' if json_results == None else "{0:,.1f}".format((size/json_results[1] - 1) * 100) + '%'
     read_delta  = '' if json_results == None else "{0:,.1f}".format((avg_read/json_results[3] - 1) * 100) + '%'
     write_delta = '' if json_results == None else "{0:,.1f}".format((avg_write/json_results[5] - 1) * 100) + '%'
@@ -82,7 +108,33 @@ def get_results (type, size, number_of_tests, repeat, read_function, write_funct
             avg_read,
             read_delta,
             avg_write,
-            write_delta]
+            write_delta,
+            sd_read,
+            min_read,
+            max_read,
+            sd_write,
+            min_write,
+            max_write]
+
+def run_repeat ():
+    json_results    = get_results('json', os.path.getsize (json_file_in), read_json_file, write_json_file)
+    bson_results    = get_results('bson', os.path.getsize (bson_file_in), read_bson_file, write_bson_file, json_results)
+    lz4_results     = get_results('lz4',  os.path.getsize (lz4_file_in), read_lz4_file, write_lz4_file, json_results)
+    zlib_results    = get_results('zlib', os.path.getsize (zlib_file_in), read_zlib_file, write_zlib_file, json_results)
+    print ('using stupid') if (stupid_flag) else print ('using timeit.repeat')
+    print ('test run ', repeat, ' times')
+    results         = PrettyTable(['type', 'file size', 'compression', 'read time', 'read %', 'write time', 'write %']) 
+    results.add_row (format_results(json_results))
+    results.add_row (format_results(bson_results))
+    results.add_row (format_results(lz4_results))
+    results.add_row (format_results(zlib_results))
+    print (results)
+    results         = PrettyTable(['type', 'read sd', 'read min', 'read max', 'write sd', 'write min', 'write max']) 
+    results.add_row (format_stats(json_results))
+    results.add_row (format_stats(bson_results))
+    results.add_row (format_stats(lz4_results))
+    results.add_row (format_stats(zlib_results))
+    print (results)
 
 if __name__ == "__main__":
     json_dict        = read_json_file ()
@@ -96,13 +148,7 @@ if __name__ == "__main__":
         write_zlib_file()
         os.rename(zlib_file_out, zlib_file_in)
     zlib_dict       = read_zlib_file ()
-    json_results    = get_results('json', os.path.getsize (json_file_in), number_of_tests, repeat, read_json_file, write_json_file)
-    bson_results    = get_results('bson', os.path.getsize (bson_file_in), number_of_tests, repeat, read_bson_file, write_bson_file, json_results)
-    lz4_results     = get_results('lz4',  os.path.getsize (lz4_file_in), number_of_tests, repeat, read_lz4_file, write_lz4_file, json_results)
-    lzib_results    = get_results('zlib', os.path.getsize (zlib_file_in), number_of_tests, repeat, read_zlib_file, write_zlib_file, json_results)
-    results         = PrettyTable(['type', 'file size', 'compression', 'read time', 'read %', 'write time', 'write %']) 
-    results.add_row (format_results(json_results))
-    results.add_row (format_results(bson_results))
-    results.add_row (format_results(lz4_results))
-    results.add_row (format_results(lzib_results))
-    print (results)
+    run_repeat ()
+    stupid_flag     = True
+    run_repeat ()
+    
--- src/load_trade.py
+++ src/load_trade.py
@@ -1,21 +1,12 @@
+from pydantic import ValidationError
 from cdm_util import cdm_trade_from_file
 
 print('loading trade from file system')
 #trade              = cdm_trade_from_file('ird-ex01-vanilla-swap_4_3.json')
-trade              = cdm_trade_from_file('LargeCDM.json')
-
-contractualProduct = trade.tradableProduct.product.contractualProduct
-economic_terms     = contractualProduct.economicTerms
-if (contractualProduct.productTaxonomy[0].productQualifier != 'InterestRate_IRSwap_FixedFloat'):
-	print('Not InterestRate_IRSwap_FixedFloat')
-elif (len(economic_terms.payout.interestRatePayout) != 2):
-	print('Not a two legged swap')
-else:
-	float_idx          = 0 if (economic_terms.payout.interestRatePayout[0].rateSpecification.fixedRate == None) else 1
-	fixed_idx          = 1 if (float_idx == 0) else 1
-	float_payout       = economic_terms.payout.interestRatePayout[float_idx]
-	fixed_payout       = economic_terms.payout.interestRatePayout[fixed_idx]
-	float_pq           = trade.tradableProduct.tradeLot[0].priceQuantity[float_idx]
-	fixed_pq           = trade.tradableProduct.tradeLot[0].priceQuantity[fixed_idx]
-	print('effective date: ' + str(float_payout.calculationPeriodDates.effectiveDate.adjustableDate.unadjustedDate))
-	print('termination date: ' + str(float_payout.calculationPeriodDates.terminationDate.adjustableDate.unadjustedDate))
+try:
+    trade           = cdm_trade_from_file('LargeCDM.json')
+    contractualProduct = trade.tradableProduct.product.contractualProduct
+    economic_terms     = contractualProduct.economicTerms
+except ValidationError as exc:
+    print(exc)
+    print(repr(exc.errors()[0]['type']))
